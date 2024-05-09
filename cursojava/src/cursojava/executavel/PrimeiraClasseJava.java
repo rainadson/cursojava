0cbf85dee7c441e6a02646e83930326e9a4f5817
raina <raina@192.168.0.7>
2024-05-09 18:57:23 -0300

    Ajudando amigo codar, primeiro Input

---
+++ cursojava/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+import javax.swing.JOptionPane;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual seu nome");
+		
+		System.out.println(nome);
+		
+		}
+
+}
